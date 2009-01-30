56f4da273144ecaf25107d740ce353569ed61578
andrews <andrews@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2009-01-30 13:04:59 +0000

    fix wrong variable in error output

--- tuntap_linux.c
+++ tuntap_linux.c
@@ -69,7 +69,7 @@ int tuntap_open(tuntap_dev *device,
 
   device->fd = open(tuntap_device, O_RDWR);
   if(device->fd < 0) {
-    printf("ERROR: ioctl() [%s][%d]\n", strerror(errno), rc);
+    printf("ERROR: ioctl() [%s][%d]\n", strerror(errno), errno);
     return -1;
   }
 
